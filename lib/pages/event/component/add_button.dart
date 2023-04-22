79db1af06ac485796f75231168bb2c5e20e49105
chocolate-kuma <natsumi0814okada@gmail.com>
2023-04-22 19:44:03 +0900

    add button

---
+++ lib/pages/event/component/add_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:jinbee/constant/color_constant.dart';
+import 'package:jinbee/pages/event/add_event_page.dart';
+
+class AddButton extends StatelessWidget {
+  const AddButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      width: 80,
+      child: FloatingActionButton(
+        onPressed: () {        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const AddEventPage()));
+},
+        backgroundColor: ColorConstants.accentColor,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
