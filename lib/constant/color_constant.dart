88db049aaeee8f595fe26151eaad89420f611356
chocolate-kuma <natsumi0814okada@gmail.com>
2023-04-22 14:35:06 +0900

    add accent color

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorConstants {
   static Color backgroundColor = const Color(0xFF14202A);
   static Color textColor = const Color(0xFFFFFFFF);
+  static Color accentColor = const Color(0xFF1E9AF1);
 }
